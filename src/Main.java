2f96d6a466305db3d6d1daf2d15e5c68fe6ce334
VladSoap <@pLUnkmdsV43TX9>
2023-06-12 07:11:47 +0300

    hw3/1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,64 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Vehicle[] vehicles = new Vehicle[2];
+        vehicles[0] = new Car("Toyota");
+        vehicles[1] = new Truck("Ford");
+        for (Vehicle vehicle : vehicles) {
+            vehicle.drive();
+        }
+        //Task 2
+        try {
+            SecondClass secondClass = new SecondClass();
+            secondClass.Method();
+        } catch (ClassCastException e) {
+            System.out.println(e);
+        }
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+}
+// Task 1
+class Vehicle {
+    private String brand;
+    public Vehicle(String brand) {
+        this.brand = brand;
+    }
+    public void drive() {
+        System.out.println("Brand of vehicle " + brand + " drives");
+    }
+    public String getBrand() {
+        return brand;
+    }
+}
+class Car extends Vehicle {
+    public Car(String brand) {
+        super(brand);
+    }
+    @Override
+    public void drive() {
+        System.out.println("Car of " + getBrand() + " drives");
+    }
+}
+class Truck extends Vehicle {
+    public Truck(String brand) {
+        super(brand);
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    @Override
+    public void drive() {
+        System.out.println("Truck of " + getBrand() + " drives");
+    }
+}
+// Task 2
+class FirstClass {
+}
+class SecondClass extends FirstClass {
+    public void Method() {
+        try {
+            FirstClass firstClass = new FirstClass();
+            SecondClass secondClass = (SecondClass) firstClass;
+        } catch (ClassCastException e) {
+            System.out.println("Error!");
         }
     }
 }
