5922dce5c5a293c25c21ef179389d2a0ccc6eb2f
VladSoap <@pLUnkmdsV43TX9>
2023-07-11 08:59:35 +0300

    homework12

---
+++ src/homework12/Lesson5.java
@@ -0,0 +1,9 @@
+package homework12;
+
+public class Lesson5 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 0; i = i - 5) {
+            System.out.println("Hello " + i);
+        }
+    }
+}
