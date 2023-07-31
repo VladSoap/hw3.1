084108b67428293c64034de60c64d2ecdbab996c
VladSoap <135171361+VladSoap@users.noreply.github.com>
2023-07-31 13:50:55 +0300

    Delete Lesson32.java

--- src/homework18/Lesson32/Lesson32.java
+++
@@ -1,9 +0,0 @@
-package homework18.Lesson32;
-
-public class Lesson32 {
-    public static void main(String[] args) {
-        Animal a = new Animal();
-        Dog dog = (Dog) a;
-        dog.bark();
-    }
-}
