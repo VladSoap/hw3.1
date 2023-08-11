8c44b6159b47bacd4c31993b9b347f9994d91482
VladSoap <135171361+VladSoap@users.noreply.github.com>
2023-08-11 08:36:04 +0300

    Delete Lesson43.java

--- src/homework20/Lesson43/Lesson43.java
+++
@@ -1,8 +0,0 @@
-package homework20.Lesson43;
-
-public class Lesson43 {
-    public static void main(String[] args) {
-        Electrocar electrocar = new Electrocar(1);
-        electrocar.start();
-    }
-}
