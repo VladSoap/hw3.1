4f2be9bb3395097806d9c6c7cfcd7881dd16f856
VladSoap <135171361+VladSoap@users.noreply.github.com>
2023-08-11 08:34:58 +0300

    Delete Animal.java

--- src/homework20/Lesson41/Animal.java
+++
@@ -1,9 +0,0 @@
-package homework20.Lesson41;
-
-public abstract class Animal {
-    public void eat() {
-        System.out.println("Im eating...");
-    }
-
-    public abstract void makeSound();
-}
